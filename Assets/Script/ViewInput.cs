ce5adfa8a65c226d0275aa08c6c4a7f5024fb518
kerrot <kasu>
2017-12-12 19:24:04 +0900

    update model

--- Assets/Script/ViewInput.cs
+++ Assets/Script/ViewInput.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ViewInput : MonoBehaviour {
+	[SerializeField] float rTrigger1;
+	[SerializeField] float rTrigger2;
+	[SerializeField] float lTrigger1;
+	[SerializeField] float lTrigger2;
+
 	[SerializeField] float moveRate = 1;
 	[SerializeField] float rotateRate = 1;
 
@@ -18,6 +23,42 @@ public class ViewInput : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		if (OVRInput.GetDown(OVRInput.RawButton.A)) {
+			Debug.Log("Aボタンを押した");
+		}
+		if (OVRInput.GetDown(OVRInput.RawButton.B)) {
+			Debug.Log("Bボタンを押した");
+		}
+		if (OVRInput.GetDown(OVRInput.RawButton.X)) {
+			Debug.Log("Xボタンを押した");
+		}
+		if (OVRInput.GetDown(OVRInput.RawButton.Y)) {
+			Debug.Log("Yボタンを押した");
+		}
+		if (OVRInput.GetDown(OVRInput.RawButton.Start)) {
+			Debug.Log("メニューボタン（左アナログスティックの下にある）を押した");
+		}
+
+		if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)) {
+			Debug.Log("右人差し指トリガーを押した");
+		}
+		if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger)) {
+			Debug.Log("右中指トリガーを押した");
+		}
+		if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger)) {
+			Debug.Log("左人差し指トリガーを押した");
+		}
+		if (OVRInput.GetDown(OVRInput.RawButton.LHandTrigger)) {
+			Debug.Log("左中指トリガーを押した");
+		}
+
+
+		rTrigger1 = OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger);
+		rTrigger2 = OVRInput.Get(OVRInput.RawAxis1D.RHandTrigger);
+		lTrigger1 = OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger);
+		lTrigger2 = OVRInput.Get(OVRInput.RawAxis1D.LHandTrigger);
+
+
 		float dx = Input.GetAxis ("Horizontal");
         float dy = Input.GetAxis ("Vertical");
 
