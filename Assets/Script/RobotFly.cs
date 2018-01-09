a74e608263b4b9ab43818a249185addfd48e934e
kerrot <k>
2018-01-09 18:48:34 +0900

    Water scene, wave adjustion, skybox

---
+++ Assets/Script/RobotFly.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UTJ;
+
+public class RobotFly : MonoBehaviour {
+	
+	[SerializeField] private float waveTime = 0.1f;
+	[SerializeField] private float waveSize = -0.55f;
+	[SerializeField] private float waveRange = 1.6f;
+	[SerializeField] private float flySpeed = 1f;
+
+
+	private float last = 0f;
+	private Vector3 inputPosition;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		inputPosition.z += flySpeed;
+		inputPosition.z %= 100;
+		inputPosition.y = transform.position.y;
+		transform.position = inputPosition;
+
+
+		if (Time.time - last > waveTime)
+		//if (Input.GetKeyDown(KeyCode.Space))
+		{
+			inputPosition.y = 0;
+			WaterSurface.Instance.makeBump(ref inputPosition, waveSize, waveRange);
+
+			last = Time.time;
+		}
+	}
+}
--- Assets/Water/Scripts/WaterManager.cs
+++ Assets/Water/Scripts/WaterManager.cs
@@ -10,18 +10,16 @@ public class WaterManager : MonoBehaviour {
     public bool water_surface_line_render_ = false;
     public Material water_surface_input_material_;
     public Material water_splash_material_;
-
-    public GameObject inputObj;
+	public float water_speed = 0.02f;
+	public Camera traceCamera;
 
     private int rendering_front_ = 0;
     private float update_time_ = 0.0f;
 
-    private Vector3 inputPosition;
-
     // Use this for initialization
     void Start () {
         WaterSurface.Instance.init(water_surface_input_material_, water_surface_distortion_, water_surface_line_render_);
-        WaterSurfaceRenderer.Instance.init(Camera.main.transform);
+        WaterSurfaceRenderer.Instance.init(traceCamera.transform);
         WaterSplash.Instance.init(water_splash_material_, WaterSurfaceRenderer.Instance.getReflectionTexture());
         WaterSplashRenderer.Instance.init(WaterSplash.Instance);
     }
@@ -33,14 +31,8 @@ public class WaterManager : MonoBehaviour {
 
         WaterSurface.Instance.render(rendering_front_);
         WaterSurfaceRenderer.Instance.render(update_time_);
-        WaterSplash.Instance.render(rendering_front_, Camera.main, update_time_);
+        WaterSplash.Instance.render(rendering_front_, traceCamera, update_time_);
 
-        update_time_ += 0.02f;
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            inputPosition = inputObj.transform.position;
-            WaterSurface.Instance.makeBump(ref inputPosition, -0.55f /* value */, 1.6f /* size */);
-        }
+        update_time_ += water_speed;
     }
 }
