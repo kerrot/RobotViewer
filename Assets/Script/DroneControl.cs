436bc6cb33155a2e8c06acc9f506580f271f6612
kerrot <kasu>
2018-01-16 19:20:18 +0900

    add sound, Drone

---
+++ Assets/Script/DroneControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneControl : MonoBehaviour {
+
+	[SerializeField] private GameObject Hit;
+	[SerializeField] private GameObject Model;
+	[SerializeField] private float scaleSize = 1.5f;
+	[SerializeField] private float viewRange = 1f;
+
+	public static DroneControl Current = null;
+
+	private void Awake( ) 
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		RaycastHit hit;
+        
+        if (Physics.SphereCast(Camera.main.transform.position, viewRange, Camera.main.transform.forward, out hit)) 
+		{
+        	
+        }
+		else
+		{
+
+		}
+	}
+}
